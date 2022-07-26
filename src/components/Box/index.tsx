d0b69f9f1995c2d6a7700a028b52174d4eb089f4
breno.ferrari <breno.ferrari@fiap.com.br>
2022-07-26 18:45:46 -0300

    sliderBar component

--- src/components/Box/index.tsx
+++ src/components/Box/index.tsx
@@ -6,15 +6,14 @@ import ToggleSwitch from "components/ToggleSwitch"
 export default function Box(){
   return ( 
     <main className={styles.container}>
+       <ToggleSlide /> 
         <div className={styles.container__middle}>
-            <ToggleSlide /> 
             <p className={styles.container__middle__text}>Montlhy Billing</p>
             <ToggleSwitch />
             <p className={styles.container__middle__text}>Yarly Billing</p>
             <div className={styles.container__middle__discount}>
                 <p className={styles.container__middle__discount__text}>25% discount</p>
             </div>
-            
         </div>
         <div className={styles.container__bottom}>
             <div className={styles.container__bottom__left}>
--- src/components/ToggleSlide/index.tsx
+++ src/components/ToggleSlide/index.tsx
@@ -1,8 +1,26 @@
+import { useState } from "react";
 import styles from "./toggle.module.scss"
 export default function ToggleSlide(){
+  const [value, setValue] = useState(0);
+  const MAX = 100;
+
+  const getBackgroundSize = () => {
+    return {
+      backgroundSize: `${(value * 100) / MAX}% 100%`,
+    };
+  };
+
   return ( 
     <div className={styles.container}>
-       
+       <input
+       className={styles.container__slider}
+        type="range"
+        min="0"
+        max={MAX}
+        onChange={(e) => setValue(e.target.valueAsNumber)}
+        style={getBackgroundSize()}
+        value={value}
+          />
     </div>
   )
 }
